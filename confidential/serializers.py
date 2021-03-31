6b9322e4410b1d1c12f54006495aace41da7d8dd
codebolab <jose.zambrana@webeando.me>
2021-03-31 17:08:02 -0400

    create endpoint with tests (#8)
    Co-authored-by: Jose Maria Zambrana Arze <contact@josezambrana.com>

--- confidential/serializers.py
+++ confidential/serializers.py
@@ -1,8 +1,33 @@
+import logging
+from datetime import timedelta
 from rest_framework import serializers
 from confidential.models import Secret
+from confidential.utils import encrypt
+
+
+logger = logging.getLogger('project')
 
 
 class ConfidentialSerializer(serializers.ModelSerializer):
+    content = serializers.CharField(write_only=True)
+    key = serializers.CharField(write_only=True, allow_null=True,
+                                allow_blank=True, required=False, default='')
+    lifetime = serializers.SerializerMethodField()
+
     class Meta:
         model = Secret
-        fields = '__all__'
+        fields = ['id', 'created', 'content', 'encrypted', 'lifetime', 'key']
+        read_only_fields = ['encrypted']
+
+    def get_lifetime(self, instance):
+        return instance.created + timedelta(hours=instance.lifetime)
+
+    def create(self, validated_data):
+        key = validated_data.pop('key')
+
+        validated_data['encrypted'] = False
+        if key:
+            validated_data['content'] = encrypt(validated_data['content'], key)
+            validated_data['encrypted'] = True
+
+        return super().create(validated_data)
--- confidential/tests.py
+++ confidential/tests.py
@@ -1,17 +1,88 @@
 import logging
 from django.urls import reverse
 from rest_framework.test import APITestCase
+from confidential.models import Secret
+from confidential.utils import encrypt, decrypt
 
 
 logger = logging.getLogger('project')
 
 
+class TestEncrypDecrypt(APITestCase):
+    def test_encrypt_decrypt(self):
+        secret = 'secret content lorem ipsum'
+        key = 'secretkey'
+
+        encrypted = encrypt(secret, key)
+        self.assertNotEquals(secret, encrypted)
+
+        decrypted = decrypt(encrypted, key)
+        self.assertEquals(secret, decrypted)
+
+
 class TestConfidential(APITestCase):
-    def test_create_secret(self):
+    def _assertCreate(self, data):
         url = reverse('confidential')
-        response = self.client.post(url, data={
+        response = self.client.post(url, data=data, format='json')
+        response_json = response.json()
+        logger.info(f" response_json: {response_json}")
+        self.assertEquals(response.status_code, 201)
+        self.assertIn("id", response_json)
+        self.assertIn("created", response_json)
+        self.assertIn("lifetime", response_json)
+        self.assertIn("encrypted", response_json)
+        self.assertNotIn("content", response_json)
+
+        return response_json
+
+    def test_create_secret(self):
+        # Create a secret with an empty key
+        data = {
             'content': 'lorem ipsum',
             'lifetime': 7 * 24,
             'key': '',
-        })
-        self.assertEquals(response.status_code, 201)
+        }
+        response_json = self._assertCreate(data)
+
+        secret = Secret.objects.get(pk=response_json['id'])
+        self.assertFalse(secret.encrypted)
+        self.assertEquals(secret.content, data['content'])
+
+        # Create a secret with key null
+        data = {
+            'content': 'lorem ipsum',
+            'lifetime': 7 * 24,
+            'key': None,
+        }
+        response_json = self._assertCreate(data)
+
+        secret = Secret.objects.get(pk=response_json['id'])
+        self.assertFalse(secret.encrypted)
+        self.assertEquals(secret.content, data['content'])
+
+        # Create a secret with a key
+        data = {
+            'content': 'lorem ipsum',
+            'lifetime': 7 * 24,
+        }
+        response_json = self._assertCreate(data)
+
+        secret = Secret.objects.get(pk=response_json['id'])
+        self.assertFalse(secret.encrypted)
+        self.assertEquals(secret.content, data['content'])
+
+    def test_create_secret_encrypted(self):
+        key = 'secretkey'
+        data = {
+            'content': 'lorem ipsum',
+            'lifetime': 7 * 24,
+            'key': key,
+        }
+
+        response_json = self._assertCreate(data)
+        secret = Secret.objects.get(pk=response_json['id'])
+        self.assertTrue(secret.encrypted)
+        self.assertNotEquals(secret.content, data['content'])
+
+        decrypted = decrypt(secret.content, key)
+        self.assertEquals(decrypted, data['content'])
