e1ef9ea0523bcec2c40a85bfe72e22b824cf4f10
codebolab <jose.zambrana@webeando.me>
2021-04-01 14:49:03 -0400

    Feature/4 : Form mockup to create a secret (#16)
    * adding sass and bootstrap
    
    * form mockup to create a secret
    
    Co-authored-by: Jose Maria Zambrana Arze <contact@josezambrana.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
+
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Container>
+            <Row>
+                <Col className="text-center">
+                    <img src={logo} className="App-logo" alt="logo" width="150" />
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Form>
+                        <Form.Group controlId="content">
+                            <Form.Label>Secreto</Form.Label>
+                            <Form.Control as="textarea" rows="3" />
+                        </Form.Group>
+                        <Form.Row>
+                            <Form.Group as={Col} controlId="lifetime">
+                                <Form.Label>Válido por</Form.Label>
+                                <Form.Control as="select">
+                                    <option value="1">1 hora</option>
+                                    <option value="1">2 horas</option>
+                                    <option value="1">3 horas</option>
+                                    <option value="24">1 día</option>
+                                    <option value="48">2 días</option>
+                                    <option value="720">7 días</option>
+                                    <option value="720">30 días</option>
+                                </Form.Control>
+                            </Form.Group>
+                            <Form.Group as={Col} controlId="key">
+                                <Form.Label>Palabra cláve</Form.Label>
+                                <Form.Control type="password" />
+                            </Form.Group>
+                        </Form.Row>
+                    <Button variant="primary" type="submit">Crear</Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default App;
